a7040eb24a8ef89577e5fa2fb54b8b71bd4665b5
Matt <mmart238@lion.lmu.edu>
2024-10-08 16:02:32 -0700

    codesandbox transfer

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Title from "./Title.jsx";
+import Entry from "./Entry.jsx";
+import Info from "./Info.jsx";
 
-import './App.css'
+export default function App() {
+  const [name, setName] = useState("");
+  const [data, setData] = useState("");
 
-function App() {
-  return <h1>Hello, World!!</h1>;
+  useEffect(() => {
+    const pokemon = encodeURIComponent(name.toLowerCase());
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+    fetch(url)
+      .then((r) => r.json())
+      .then((r) => setData(r))
+      .catch((e) => setData(`${e}`));
+  }, [name]);
+
+  return (
+    <div className="App">
+      <Title text="Poké Finder" />
+      <Entry action={setName} />
+      <Info name={name} data={data} />
+    </div>
+  );
 }
-
-export default App
